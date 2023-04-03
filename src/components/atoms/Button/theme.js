5add0dfaf0af1613a6e296c68c8f4142c25f6cda
abuhajaar <adambis28@gmail.com>
2023-04-03 16:10:56 +0200

    update dark mode

---
+++ src/components/atoms/Button/theme.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { ThemeConsumer } from '../../../contexts/ThemeContext';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+function ToggleTheme() {
+  return (
+    <div>
+      <ThemeConsumer>
+        {({ theme, toggleTheme }) => {
+          return (
+            <button onClick={toggleTheme}>
+              {theme === 'light' ? <FaMoon /> : <FaSun />}
+            </button>
+          );
+        }}
+      </ThemeConsumer>
+    </div>
+  );
+}
+
+export default ToggleTheme;
--- src/components/molekules/header/index.js
+++ src/components/molekules/header/index.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './header.scss';
 import { Link } from 'react-router-dom';
-import logo1 from '../../../assets/image/logo1.png'
+import logo1 from '../../../assets/image/logo1.png';
 import Gap from '../../atoms/Gap';
-import { Button } from './../../../components'
+import { Button } from './../../../components';
+import ToggleTheme from '../../atoms/Button/theme';
 
 function Header({ LogOut }) {
-
-    return (
-        <div className='container'>
-            <Link className='Link' to={'/'} >
-                <img className='logo1' src={logo1} alt='logo' />
-                <Gap width={10} />
-                <h1>Catatan</h1>
-            </Link>
-            <Link className='logo22' to={'/arsip'} >
-                <h2>Arsip</h2>
-            </Link>
-            <Button onClick={LogOut} title={'Log Out'} ></Button>
-        </div >
-    )
+  return (
+    <div className="container">
+      <Link className="Link" to={'/'}>
+        <img className="logo1" src={logo1} alt="logo" />
+        <Gap width={10} />
+        <h1>Catatan</h1>
+      </Link>
+      <Link className="logo22" to={'/arsip'}>
+        <h2>Arsip</h2>
+      </Link>
+      <ToggleTheme />
+      <Button onClick={LogOut} title={'Log Out'}></Button>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
